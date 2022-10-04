0f4979ed27354802388f15e311a5355e210f0501
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-05 01:58:31 +0530

    Navigating To Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,26 @@
+import Link from "next/link";
 function ClientsPage() {
+  const clients = [
+    { id: "c1", name: "Rahul" },
+    { id: "c2", name: "Ravi" },
+    { id: "c3", name: "Itnesh" },
+  ];
   return (
     <div>
       <h1>The Client Page</h1>
+      <ul>
+        {clients.map((el) => (
+          <li key={el.id}>
+            <Link href={`/clients/${el.name}`}>{el.name}</Link>
+          </li>
+        ))}
+        {/* <li>
+          <Link href="/clients/Rahul">Rahul</Link>
+        </li>
+        <li>
+          <Link href="/clients/Rohan">Rohan</Link>
+        </li> */}
+      </ul>
     </div>
   );
 }
