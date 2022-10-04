3fa893a57c90d7eabe2dd0ac72f32c03da8ffcd8
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-05 02:08:50 +0530

    Navigating Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,9 +2,19 @@ import { useRouter } from "next/router";
 function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query);
+
+  function loadProjectHandler() {
+    //  load data...
+    /* router.push("clients/Rahul/projecta"); */
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "Rahul", clientprojectid: "projecta" },
+    });
+  }
   return (
     <div>
       <h1>The Project of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
