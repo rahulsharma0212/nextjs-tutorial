c1e95d8d65135579c51fcd1caf0110a6b79a3ecb
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-05 01:50:46 +0530

    Navigating with the Link Component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 function HomePage() {
   return (
     <div className={styles.container}>
       <h1>This is a home page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
