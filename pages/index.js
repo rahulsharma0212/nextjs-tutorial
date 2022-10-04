c19165ac4a1e154a39834af63a70d1eb8fa26ee3
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-05 00:45:44 +0530

    Adding A First Page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+function HomePage() {
   return (
     <div className={styles.container}>
-      <h1>Hello Next World!</h1>
+      <h1>This is a home page</h1>
     </div>
   );
 }
+
+export default HomePage;
