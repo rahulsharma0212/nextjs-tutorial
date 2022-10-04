05a3b5dff86bc1b4ff8c4324da84169471a8ad8a
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-05 00:54:26 +0530

    Working with Nested Paths & Routes

---
+++ pages/portfolio/index.js
@@ -0,0 +1,9 @@
+function PortfolioPage() {
+  return (
+    <div>
+      <h1>The portfolio page</h1>
+    </div>
+  );
+}
+
+export default PortfolioPage;
---
+++ pages/portfolio/list.js
@@ -0,0 +1,9 @@
+function ListPage() {
+  return (
+    <div>
+      <h1>The list page</h1>
+    </div>
+  );
+}
+
+export default ListPage;
