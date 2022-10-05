fe12e97fa5ebf47a88e3f3cf31905bf87812d46c
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-05 08:43:00 +0530

    Adding More React Components & Connecting Components

--- components/events/EventList.js
+++ components/events/EventList.js
@@ -3,8 +3,15 @@ function EventList(props) {
   const { items } = props;
   return (
     <ul>
-      {items.map((events) => (
-        <EventItem />
+      {items.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
       ))}
     </ul>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/EventList";
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>The Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
