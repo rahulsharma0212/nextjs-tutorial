af21ce6b2c16e380aef4b0d2f37e52e0b0ee39dd
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-05 10:32:30 +0530

    Adding a General Layout Wrapper Component

---
+++ components/layout/layout.js
@@ -0,0 +1,11 @@
+import MainHeader from "./main-header";
+function Layout(props) {
+  return (
+    <>
+      <MainHeader />
+      <main>{props.children}</main>
+    </>
+  );
+}
+
+export default Layout;
