09fc3516c2b594deec958184c037333ead137e6f
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-05 20:05:17 +0530

    Using getStaticPaths

--- pages/[pid].js
+++ pages/[pid].js
@@ -27,4 +27,15 @@ export async function getStaticProps(context) {
     },
   };
 }
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { pid: "p1" } },
+      { params: { pid: "p2" } },
+      { params: { pid: "p3" } },
+    ],
+    fallback: false,
+  };
+}
 export default ProductDeatailPage;
