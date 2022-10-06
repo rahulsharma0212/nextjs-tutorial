8fc6e9da9512aec3c682c359667988bd79238abb
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-06 22:57:28 +0530

    Working With Fallback Pages

--- pages/[pid].js
+++ pages/[pid].js
@@ -3,6 +3,10 @@ import path from "path";
 
 function ProductDeatailPage(props) {
   const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
   return (
     <>
       <h1>{loadedProduct.title}</h1>
@@ -32,10 +36,10 @@ export async function getStaticPaths() {
   return {
     paths: [
       { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
+      // { params: { pid: "p2" } },
+      // { params: { pid: "p3" } },
     ],
-    fallback: false,
+    fallback: true, // true,false,"blocking"
   };
 }
 export default ProductDeatailPage;
