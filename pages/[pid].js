1f861cc52d290f8c69cd3236eeda6269f2bfb00a
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-06 23:13:27 +0530

    Loading Paths Dynamically

--- pages/[pid].js
+++ pages/[pid].js
@@ -15,13 +15,17 @@ function ProductDeatailPage(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const productId = params.pid;
-
+async function getData() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+  const data = await getData();
 
   const product = data.products.find((el) => el.id === productId);
 
@@ -33,13 +37,23 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const chngData = data.products.map((el) => ({
+    params: {
+      pid: el.id,
+    },
+  }));
   return {
+    paths: chngData,
+    fallback: false, // true,false,"blocking"
+  };
+  /* return {
     paths: [
       { params: { pid: "p1" } },
       // { params: { pid: "p2" } },
       // { params: { pid: "p3" } },
     ],
     fallback: true, // true,false,"blocking"
-  };
+  }; */
 }
 export default ProductDeatailPage;
