ca900b75100b76364e2920f57d35d8187cfb8b20
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-08 08:47:49 +0530

    getServerSideProps Behind The Scenes after build

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -6,6 +6,7 @@ export default UserProfilePage;
 
 export async function getServerSideProps(context) {
   const { params, req, res } = context;
+  console.log("server side code");
   return {
     props: {
       userName: "Rahul",
