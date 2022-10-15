d3db190c28c515b5ac4d9c8af6744801bd1a4e06
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-15 11:24:41 +0530

    Optimizing Images with the Next Image Component & Feature

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -3,6 +3,7 @@ import Button from "../ui/Button";
 import DateIcon from "../icons/date-icon";
 import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
+import Image from "next/image";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -15,7 +16,7 @@ function EventItem(props) {
   const exploreLink = `/events/${id}`;
   return (
     <li className={classes.item}>
-      <img src={`/${image}`} alt={title} />
+      <Image src={`/${image}`} alt={title} width={340} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
