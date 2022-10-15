9dfd8c0829fa4eb6d4b63a0cb1de90ccc9a66f0e
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-15 10:55:07 +0530

    The _document.js File (And What It Does)

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const document = () => (
+  <Html lang="en">
+    <Head />
+    <body>
+      <div id="overlay"></div>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default document;
