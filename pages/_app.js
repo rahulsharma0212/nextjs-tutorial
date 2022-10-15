b466f6570d1d07ac71bd9a211aeac1f0f2187267
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-15 10:38:53 +0530

    Working with the _app.js File (and Why)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import Layout from "../components/layout/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta
+          name="viewport"
+          content=" initial-scale=1.0 , width=device-width"
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
